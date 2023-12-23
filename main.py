4a3a1cce5e9fccdcf0bae0ed5813848908e04896
karturbis <vulkanera42@gmx.ch>
2023-12-23 03:53:53 +0100

    move ortsfaktor from main to rocket_fuel_calculation and add the mathematical function to it

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import rocket_fuel_calculation
 import cli
 
-ortsfaktor = 9,81
-eulerzahl = 2,71828182845904523536028747135266249775724709369995
+
 
 if __name__ == "__main__":
     cli.cli()
--- rocket_fuel_calculation.py
+++ rocket_fuel_calculation.py
@@ -1,6 +1,16 @@
+import math
+
+ortsfaktor = 9.81
+
+
+
 def calc0():
     pass
 
-def calc1(fschub, isp, zielhoehe, leermasse):
+
+def calc1(fschub:float, isp:float, zielhoehe:float, leermasse:float) -> float:
+
     #Masse an benötigtem Raketentreibstoff = m1*e^(sqrt(2*g*h1)/Isp*g)
-    print("Hello world")
+    return (leermasse*math.exp(math.sqrt(2*ortsfaktor*zielhoehe)/isp*ortsfaktor))
+
+    
