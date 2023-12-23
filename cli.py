65efe685e52590818fabd2ebb802849122bc73de
karturbis <vulkanera42@gmx.ch>
2023-12-23 03:32:40 +0100

    add call to method rfc call

--- cli.py
+++ cli.py
@@ -1,13 +1,15 @@
 import rocket_fuel_calculation
 
 def cli():
-    genauigkeit = input("Bitte geben sie für die genauigkeit der Berechnung eine Zahl zwischen 1 und 1 ein\n>> ")
+    genauigkeit = int(input("Bitte geben sie für die genauigkeit der Berechnung eine Zahl zwischen 1 und 1 ein\n>> "))
 
     if genauigkeit >=1:
-        fschub = input("Bitte geben sie den Schub in Newton ein welchen der Raketenantrieb erzeugt.\n>> ")
-        isp = input("Bitte geben sie den Spezifischen Impuls ihres Raketentriebwerkes in Sekunden ein.\n>> ")
-        zielhoehe = input("Bitte geben sie die Höhe ein, welche von ihrer Rakete erreicht werden soll. (In Metern)\n>> ")
-        leermasse = input("Bitte geben sie die Masse ihrer Rakete ohne Treibstoff ein. (In Kilogram)\n>> ")
+        fschub = float(input("Bitte geben sie den Schub in Newton ein welchen der Raketenantrieb erzeugt.\n>> "))
+        isp = float(input("Bitte geben sie den Spezifischen Impuls ihres Raketentriebwerkes in Sekunden ein.\n>> "))
+        zielhoehe = float(input("Bitte geben sie die Höhe ein, welche von ihrer Rakete erreicht werden soll. (In Metern)\n>> "))
+        leermasse = float(input("Bitte geben sie die Masse ihrer Rakete ohne Treibstoff ein. (In Kilogram)\n>> "))
+        
+    rfc_call(genauigkeit)
 
 
 
