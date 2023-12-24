bee2c257bf5c9a711e851d29cb9d1d8a6c3858bc
karturbis <vulkanera42@gmx.ch>
2023-12-24 02:52:13 +0100

    add class and constructor rocket_fuel_calculation

--- rocket_fuel_calculation.py
+++ rocket_fuel_calculation.py
@@ -1,16 +1,26 @@
 import math
 
-ortsfaktor = 9.81
+
+class rocket_fuel_calculation:
+
+    ortsfaktor = 9.81
+
+    def __init__(genauigkeit, fschub, isp, zielhoehe, leermasse):
+
+        self.genauigkeit = genauigkeit
+        self.fschub = fschub
+        self.isp = isp
+        self.zielhoehe = zielhoehe
+        self.leermasse = leermasse
 
 
+    def calc0():
+        pass
 
-def calc0():
-    pass
 
+    def calc1(fschub:float, isp:float, zielhoehe:float, leermasse:float) -> float:
 
-def calc1(fschub:float, isp:float, zielhoehe:float, leermasse:float) -> float:
+        #Masse an benötigtem Raketentreibstoff = m1*e^(sqrt(2*g*h1)/Isp*g)
+        return (leermasse*math.exp(math.sqrt(2*ortsfaktor*zielhoehe)/isp*ortsfaktor))
 
-    #Masse an benötigtem Raketentreibstoff = m1*e^(sqrt(2*g*h1)/Isp*g)
-    return (leermasse*math.exp(math.sqrt(2*ortsfaktor*zielhoehe)/isp*ortsfaktor))
-
-    
+        
