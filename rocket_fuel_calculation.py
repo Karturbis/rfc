6319cccc873220f92a70d54dcf20d2cf7501a17c
karturbis <vulkanera42@gmx.ch>
2023-12-24 15:34:14 +0100

    add self. to all varaibles in the calc1() method, fix typo in doc comment

--- rocket_fuel_calculation.py
+++ rocket_fuel_calculation.py
@@ -32,16 +32,16 @@ class Rocket_fuel_calculation:
         isp (Den Spezifischen Impuls des Triebwerkes in Sekunden)
         zielhoehe (Die Höhe, welche die Rakete erreichen soll in Metern)
         leermasse (Die Leermasse der Rakete, also ohne Treibstoff, aber mit allem anderen in Kilogram)
-        ortsfaktor (da dies nur eine Simple berechnung ist, wird für den Ortsfaktor 9.81m/s² eingesetzt)
+        Ortsfaktor (da dies nur eine Simple berechnung ist, wird für den Ortsfaktor 9.81m/s² eingesetzt)
 
         Diese Methode vernachlässigt Luftwiederstand und die Bewegung im Gravitationsfeld, ausserdem werden auch die
         verschiedenen Stufen der Rakete vernachlässigt.
         """
 
         # Berechnung der benötigten geschwindigkeitsänderung, um die Zielhöhe zu erreichen.
-        self.deltav = math.sqrt(2*ortsfaktor*zielhoehe)
+        self.deltav = math.sqrt(2*self.ortsfaktor*self.zielhoehe)
 
         # Berechnung des benötigten Treibstoffes in kg. Das Ergebnis wird an die Aufrufende methode zurückgegeben.
-        return (leermasse*math.exp(deltav/isp*ortsfaktor))
+        return (self.leermasse*math.exp(self.deltav/self.isp*self.ortsfaktor))
 
         
