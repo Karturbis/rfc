ceaa289f4246915e6373dcec921a326aa48c9287
karturbis <vulkanera42@gmx.ch>
2023-12-24 15:56:12 +0100

    add Warning for incorrect results

--- cli.py
+++ cli.py
@@ -1,14 +1,18 @@
 import rocket_fuel_calculation as rocket_fc
+from colorama import Fore, Back, Style
 
 def rfc_print(genauigkeit, fschub, isp, zielhoehe, leermasse):
-    rfc = rocket_fc.Rocket_fuel_calculation(genauigkeit, fschub, isp, zielhoehe, leermasse)
+    
     if genauigkeit == 1:
+        rfc = rocket_fc.Rocket_fuel_calculation(genauigkeit, fschub, isp, zielhoehe, leermasse)
         ergebnis = str(round(rfc.calc1(), 3))
+        
     
     print("Die Benötigte Menge an Treibstoff sind etwa: " + ergebnis + " Kilogram.")
 
 
 def cli():
+    print(f"{Fore.RED}Achtung!\nDas Programm befindet sich in der Beta Phase und gibt kein Sinnvollen Ergebnisse aus!{Style.RESET_ALL}\n")
     genauigkeit = int(input("Bitte geben sie für die genauigkeit der Berechnung eine Zahl zwischen 1 und 1 ein\n>> "))
 
     if genauigkeit >=1:
